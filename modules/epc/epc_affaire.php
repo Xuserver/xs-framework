f3f50d70631949ca43afd01705c2851b70cfc901
Xuserver <gael.jaunin@gmail.com>
2020-06-20 09:34:08 +0200

    remove comment

--- modules/epc/epc_affaire.php
+++ modules/epc/epc_affaire.php
@@ -9,18 +9,7 @@ class epc_affaire extends \xuserver\v5\model{
         return $this->properties()->find("affaire")->val();
     }
     public function test(){
-        /*
-        echo "<h1> find text find etude find</h1>";
-        $t="";
-        $this->properties->find("text","type")->each(function ($p) use($t){
-            $p->val("test");
-            echo "<br/>$t".$p->name(). $p->ui->input();
-        })->find("etude")->each(function ($p) use($t){
-            echo "<br/>$t".$p->name(). $p->ui->input();
-        })->find("user")->each(function ($p) use($t){
-            echo "<br/>$t".$p->name(). $p->ui->input();
-        });
-        */
+
         echo "<h2>FORM GENERATOR #".$this->db_tablename()."(".$this->db_id().")</h2>";
         $t="ima,id,da,descr";
         echo "<h3> find($t) </h3>";
