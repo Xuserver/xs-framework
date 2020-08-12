653ad0c66119fe19ce65315da4d93c9355247a19
Xuserver <gael.jaunin@gmail.com>
2020-08-12 18:56:59 +0200

    Update config.php

--- v5/config.php
+++ v5/config.php
@@ -10,7 +10,7 @@
 
     
     define('XUSERVER_DB_SERVER',"localhost");   // Database server internet address
-    define('XUSERVER_DB_NAME',"bjas_prod");     // Database name
+    define('XUSERVER_DB_NAME',"");     // Database name
     define('XUSERVER_DB_USERNAME',"root");   // Database username
     define('XUSERVER_DB_PASSWORD',"");  // Database password
  
